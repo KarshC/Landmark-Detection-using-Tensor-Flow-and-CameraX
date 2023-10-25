423150e3e22c5c4aa9607e22557c28f1308207c1
karshC <karshpsu@gmail.com>
2023-10-25 07:37:37 -0400

    classifying camera preview

--- app/src/main/java/com/example/landmarkdetectionusingtensorflowandcamerax/MainActivity.kt
+++ app/src/main/java/com/example/landmarkdetectionusingtensorflowandcamerax/MainActivity.kt
@@ -3,13 +3,32 @@ package com.example.landmarkdetectionusingtensorflowandcamerax
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.camera.view.CameraController
+import androidx.camera.view.LifecycleCameraController
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
+import com.example.landmarkdetectionusingtensorflowandcamerax.data.TfLiteLandmarkClassifier
+import com.example.landmarkdetectionusingtensorflowandcamerax.domain.Classification
+import com.example.landmarkdetectionusingtensorflowandcamerax.presentation.CameraPreview
+import com.example.landmarkdetectionusingtensorflowandcamerax.presentation.LandmarkImageAnalyzer
 import com.example.landmarkdetectionusingtensorflowandcamerax.ui.theme.LandmarkDetectionUsingTensorFlowAndCameraXTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +36,51 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LandmarkDetectionUsingTensorFlowAndCameraXTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                var classifications by remember {
+                    mutableStateOf(emptyList<Classification>())
+                }
+                var analyzer = remember {
+                    LandmarkImageAnalyzer(
+                        classifier = TfLiteLandmarkClassifier(
+                            context = applicationContext
+                        ),
+                        onResults = {
+                            classifications = it
+                        }
+                    )
+                }
+                var controller = remember {
+                    LifecycleCameraController(applicationContext).apply {
+                        setEnabledUseCases(CameraController.IMAGE_ANALYSIS)
+                        setImageAnalysisAnalyzer(
+                            ContextCompat.getMainExecutor(applicationContext),
+                            analyzer
+                        )
+                    }
+                }
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
                 ) {
-                    Greeting("Android")
+                    CameraPreview(controller = controller, modifier = Modifier.fillMaxSize())
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .align(Alignment.TopCenter)
+                    ) {
+                        classifications.forEach {
+                            Text(
+                                text = it.name,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(MaterialTheme.colorScheme.primaryContainer)
+                                    .padding(8.dp),
+                                textAlign = TextAlign.Center,
+                                fontSize = 20.sp,
+                                color = MaterialTheme.colorScheme.primary
+                            )
+                        }
+                    }
                 }
             }
         }
